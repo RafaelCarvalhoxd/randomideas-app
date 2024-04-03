51455f21fbf0797064331ced76dc210831d68203
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-04-02 22:28:56 -0300

    fix: models type

--- models/idea.js
+++ models/idea.js
@@ -1,23 +1,20 @@
 const mongoose = require('mongoose');
 
 const IdeaSchema = new mongoose.Schema({
-
-    text: {
-        type: String,
-        required: [true, 'Please add some text']
-    },
-    tag: {
-        type: String,
-        required: [true, 'Please add a tag']
-    },
-    username: {
-        type: String,
-        required: [true, 'Please add a username']
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+  text: {
+    type: String,
+    required: [true, 'Please add a text field'],
+  },
+  tag: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model('Idea', IdeaSchema);
