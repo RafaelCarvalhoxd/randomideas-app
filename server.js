03597893cafc68a468d62e2ad90de4a384d08611
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-04-02 22:20:16 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config(); 
+const port = process.env.PORT || 5000; 
+const connectDB = require('./config/db'); 
+
+
+connectDB(); 
+
+const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
+
+app.get('/', (req, res) => { 
+    res.json({ message: 'Welcome to the RandomIdeas API' });
+});
+
+const ideasRouter = require('./routes/ideas'); 
+app.use('/api/ideas', ideasRouter);  
+
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
